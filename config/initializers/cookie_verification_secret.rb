5f9099500caf2c24583b3be4f1391021556f9d3e
Enrique Diaz <nrikediaz@gmail.com>
2010-08-06 03:13:55 -0500

    Testing paperclip

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'a23b1eb1d9d8cfb7d9a00f1f4f8d7fdd109fba2cbeae19b0a3a9b865a2706d04cc79908dd874e018a25d8561315f928471778f039669f94d53fbf61f93d9be35';
